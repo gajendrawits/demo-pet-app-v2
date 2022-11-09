e22442cf17cff0b6df48a66dd4002643d9938cbf
pulkit.chouhan999 <pulkitchouhan99@gmail.com>
2022-11-09 20:09:35 +0530

    feat:addPet&Paginate

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/loginPage/LoginPage";
+import PrivateRouts from "./components/PrivateRouts/privateRouts";
 import "./App.css";
 import CategoriesPage from "./components/categories/CategoriesPage";
 import HomePage from "./components/homePage/HomePage";
 import ProductCart from "./components/productCart/ProductCart";
+import AddPet from "./components/addPet/AddPet";
 
 function App() {
   return (
@@ -12,9 +14,38 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route path="home" element={<HomePage />} />
-          <Route path="categories" element={<CategoriesPage />} />
-          <Route path="products" element={<ProductCart />} />
+          <Route
+            path="home"
+            element={
+              <PrivateRouts>
+                <HomePage />
+              </PrivateRouts>
+            }
+          />
+          <Route
+            path="categories"
+            element={
+              <PrivateRouts>
+                <CategoriesPage />
+              </PrivateRouts>
+            }
+          />
+          <Route
+            path="categories/products"
+            element={
+              <PrivateRouts>
+                <ProductCart />
+              </PrivateRouts>
+            }
+          />
+          <Route
+            path="addPets"
+            element={
+              <PrivateRouts>
+                <AddPet />
+              </PrivateRouts>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
--- src/validations/UserValidations.tsx
+++ src/validations/UserValidations.tsx
@@ -2,10 +2,6 @@ import * as yup from "yup";
 
 export const NewSchema = yup.object().shape({
   email: yup.string().email().required(),
-  password: yup
-    .string()
-    .min(8)
-    .max(30)
-    .required()
-    .matches(/^(?! )[A-Za-z0-9\s@]*$/, "Enter valid password"),
+  password: yup.string().min(8).max(30).required(),
+  // .matches(/^(?! )[A-Za-z0-9\s@]*$/, "Enter valid password"),
 });
